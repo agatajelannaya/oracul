ae2ec305389223f6366af9d5384eb93db6aa89f2
Svetlana Ostroumova <ostroumova095@gmail.com>
2021-07-03 19:05:20 +0300

    ispravleniya

--- oracul.go
+++ oracul.go
@@ -1,30 +1,27 @@
 package main
 
 import (
-  "fmt" 
-  "time"
-  "math/rand"
-  ) 
+	"fmt"
+	"math/rand"
+	"time"
+)
 
-func main(){
-   rand.Seed(time.Now().UnixNano())
-   x:=[]string{
-     "да", 
-     "конечно!",
-     "вероятность велика",
-     "вероятность мала",
-     "неизвестно",
-     "лучше пока не говорить тебе об этом",
-     "нет", 
-     "иди на хуй", 
-     "прости стрессы", 
-     "возлюби ближнего своего",
-     "переформулируй вопрос и задай снова",
-     "дай оракулу отдохнуть",
-     }
-   fmt.Println("Подумайте о вопросе...")
-   time.Sleep(3000 * time.Millisecond)
-   fmt.Println("Приготовьтесь!")
-   time.Sleep(2000 * time.Millisecond)
-   fmt.Println("Оракул говорит: ", x[rand.Intn(len(x))])
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	x := []string{
+		"да",
+		"конечно!",
+		"вероятность велика",
+		"вероятность мала",
+		"неизвестно",
+		"лучше пока не говорить тебе об этом",
+		"нет",
+		"возлюби ближнего своего",
+		"переформулируй вопрос и задай снова",
+	}
+	fmt.Println("Подумайте о вопросе...")
+	time.Sleep(3000 * time.Millisecond)
+	fmt.Println("Приготовьтесь!")
+	time.Sleep(2000 * time.Millisecond)
+	fmt.Println("Оракул говорит: ", x[rand.Intn(len(x))])
 }
